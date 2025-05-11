d43bb0aac1a744f6d6ad5ae0c7e0424325c300bb
Davide Bazzani <119046807+DavBazz00@users.noreply.github.com>
2025-05-11 11:14:45 +0200

    Update MAIN_Fisher_Kolmogorov.m

--- Project 82 nodes/MAIN_Fisher_Kolmogorov.m
+++ Project 82 nodes/MAIN_Fisher_Kolmogorov.m
@@ -1,11 +1,8 @@
 clear; close all; clc;
-addpath('/Users/Speranza/Desktop/VSCODE/matlab/biomedicina/82_nodes/project_82_withfunction/functions')
 
 %% 1. Load file
-
 edgeFile = 'Edge.csv';
 nodeFile = 'Node.csv';
-
 [A, CoordTable, Coord] = AdjacencyMatrix(edgeFile, nodeFile);
 
 
@@ -15,22 +12,20 @@ diffusion = 5e-4;
 a = 0.5;         % Logistic growth rate parameter, determine how fast the infection can grow
 dt = 0.4;        % Time step size in years
 num_steps = 100;
-
 % Call the infection simulation function
 [t_sol, c_sol] = FK_propagation(A, CoordTable,diffusion, a, dt, num_steps);
 
+
 %% 3. Plot Average Infection Concentration Over Time
 plotAvgInfectionConcentration(t_sol, c_sol, 'Average concentration');
+
 
 %% 3.5 3D plot of the infection distribution both at time t=0 and at a desired time
 time_desired = 20;
 plotInfectionDynamics3D(Coord, CoordTable, c_sol, t_sol, time_desired);
 
 
-%% 4. Plot average concentration by brain region overtime
-plotRegionalConcentration(t_sol, c_sol, CoordTable, 'Average concentration per region');
-
-%% 4.5 Plot Average Concentration Over Time for Specific Node Pairs
+%% 4. Plot Average Concentration Over Time for Specific Node Pairs
 plotSpecificNodePairs(t_sol, c_sol, CoordTable, 'Average Concentration Over Time for Specific Node Pairs');
 
 
@@ -38,11 +33,14 @@ plotSpecificNodePairs(t_sol, c_sol, CoordTable, 'Average Concentration Over Time
 edge_reduction = 0.20;
 [t_sol_dinamic, c_sol_dinamic] = FK_propagation_dynamic(A, CoordTable, diffusion, a, dt, num_steps, edge_reduction);
 
+
 %% 6. Plot Average Infection Concentration Over Time
 plotAvgInfectionConcentration(t_sol_dinamic, c_sol_dinamic, 'Average concentration with dynamic edge variation');
 
+
 %% 7. Comparison between first and after dynamic variation
 plotComparisonAvgConcentration(t_sol, c_sol, t_sol_dinamic, c_sol_dinamic, 'Average Infection Concentration Comparison');
+
 
 %% 8. Plot average concentration with different values of alpha
 plotMultiAlpha_FK(A, CoordTable, diffusion, dt, num_steps);
@@ -68,9 +66,7 @@ num_steps_base = round(t_end / dt_aging);
 plotPropagationWithTreatment(t_base, c_base, t_treat, c_treat, t_switch,'Baseline vs. Cure (FK)' );
 
 
-
 %% 10. Comparison of Four Scenarios (FK)
-
 plotFourScenariosFK( ...
   A, CoordTable, diffusion, a, edge_reduction, ...
   dt_aging, dt_treat, t_switch, t_end );
